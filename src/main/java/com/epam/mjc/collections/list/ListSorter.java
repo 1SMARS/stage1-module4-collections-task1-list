fd01aa021e98aaad81d89ba72c3fe4009f5be264
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-12-23 22:59:13 +0600

    Update ListSorter.java

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,25 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        
+    ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i =0;i<sourceList.size();i++){
+            arrayList.add(Integer.parseInt(sourceList.get(i)));
+        }
+        Collections.sort(arrayList,new ListComparator());
+        sourceList.clear();
+        ArrayList<String> finalArrayList = new ArrayList<>();
+        for(int i = 0;i<arrayList.size();i++){
+            finalArrayList.add(String.valueOf(arrayList.get(i)));
+        }
+        sourceList.addAll(finalArrayList);
+
     }
 }
 
-class ListComparator implements Comparator<String> {
+class ListComparator implements Comparator<Integer> {
     @Override
-    public int compare(String a, String b) {
+    public int compare(Integer first, Integer second) {
+        return Integer.compare((int)Math.pow(5*first,2)+3,(int)Math.pow(5*second,2)+3);
     }
 }
